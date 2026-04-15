1c329ace4f2a136f8b5c02178f0ce27d9b978a23
Kerk <matthew.jacobs.works@gmail.com>
2026-04-14 21:03:15 -0500

    hammer damage increased from 15 to 35

--- entities/weapons/weapon_zs_hammer/shared.lua
+++ entities/weapons/weapon_zs_hammer/shared.lua
@@ -1,7 +1,8 @@
 SWEP.Base = "weapon_zs_basemelee"
 
 SWEP.PrintName = "Carpenter's Hammer"
-SWEP.Description = "A simple but extremely useful tool. Allows you to hammer in nails to make barricades.\nPress SECONDARY FIRE to hammer in nail. It will be attached to whatever is behind it.\nPress RELOAD to take a nail out.\nUse PRIMARY FIRE to bash zombie brains or to repair damaged nails.\nYou get a point bonus for repairing damaged nails but a point penalty for removing another player's nails."
+SWEP.Description =
+	"A simple but extremely useful tool. Allows you to hammer in nails to make barricades.\nPress SECONDARY FIRE to hammer in nail. It will be attached to whatever is behind it.\nPress RELOAD to take a nail out.\nUse PRIMARY FIRE to bash zombie brains or to repair damaged nails.\nYou get a point bonus for repairing damaged nails but a point penalty for removing another player's nails."
 
 SWEP.DamageType = DMG_CLUB
 
@@ -19,8 +20,7 @@ SWEP.Secondary.ClipSize = 1
 SWEP.Secondary.DefaultClip = 1
 SWEP.Secondary.Ammo = "dummy"
 
---SWEP.MeleeDamage = 35 -- Reduced due to instant swing speed
-SWEP.MeleeDamage = 15
+SWEP.MeleeDamage = 35
 SWEP.MeleeRange = 50
 SWEP.MeleeSize = 0.875
 
@@ -51,9 +51,9 @@ function SWEP:SetNextAttack()
 end
 
 function SWEP:PlayHitSound()
-	self:EmitSound("weapons/melee/crowbar/crowbar_hit-"..math.random(4)..".ogg", 75, math.random(110, 115))
+	self:EmitSound("weapons/melee/crowbar/crowbar_hit-" .. math.random(4) .. ".ogg", 75, math.random(110, 115))
 end
 
 function SWEP:PlayRepairSound(hitent)
-	hitent:EmitSound("npc/dog/dog_servo"..math.random(7, 8)..".wav", 70, math.random(100, 105))
+	hitent:EmitSound("npc/dog/dog_servo" .. math.random(7, 8) .. ".wav", 70, math.random(100, 105))
 end
