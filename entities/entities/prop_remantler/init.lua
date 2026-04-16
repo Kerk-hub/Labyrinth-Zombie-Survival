970d6556fd8f7a248194a8c4eaa7a5aec1b5015f
3THAN123123 <ETPeachey@hotmail.com>
2026-04-16 22:25:41 +0100

    false*

--- entities/entities/prop_remantler/init.lua
+++ entities/entities/prop_remantler/init.lua
@@ -23,7 +23,7 @@ function ENT:Initialize()
 	local phys = self:GetPhysicsObject()
 	if phys:IsValid() then
 		phys:EnableMotion(false)
-		phys:EnableCollisions(flase)
+		phys:EnableCollisions(false)
 	end
 
 	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
