a696b972bc16026b1966eecb53932c4aa7c08934
3THAN123123 <ETPeachey@hotmail.com>
2026-04-17 13:22:15 +0100

    Revert "Final 🙏 fix"
    This reverts commit 8a7c22c1a0623b44d55a93c04eba5d4044031e64.

--- entities/entities/prop_obj_sigil/init.lua
+++ entities/entities/prop_obj_sigil/init.lua
@@ -8,10 +8,9 @@ function ENT:Initialize()
 
 	self:SetModel("models/props_wasteland/medbridge_post01.mdl")
 
-	self:SetSolid(SOLID_BBOX)  
+	self:SetSolid(SOLID_NONE)
 	self:SetMoveType(MOVETYPE_NONE)
 	self:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
-	self:CollisionRulesChanged()
 
 	self:SetUseType(SIMPLE_USE)
 
