bc78250f5bba9b3919136f3b4f08e7b3552349cd
Satyrios <samuel.cruz.azambuja@gmail.com>
2026-04-18 21:52:42 -0300

    increase doomcrab size

--- gamemode/zombieclasses/boss_doomcrab/shared.lua
+++ gamemode/zombieclasses/boss_doomcrab/shared.lua
@@ -22,7 +22,7 @@ CLASS.Speed = 210
 CLASS.NoFallDamage = true
 CLASS.NoFallSlowdown = true
 
-CLASS.ModelScale = 1
+CLASS.ModelScale = 1.5
 CLASS.Hull = {Vector(-16, -16, 0), Vector(16, 16, 12 * CLASS.ModelScale)}
 CLASS.HullDuck = {Vector(-16, -16, 0), Vector(16, 16, 12 * CLASS.ModelScale)}
 CLASS.ViewOffset = Vector(0, 0, 48)
